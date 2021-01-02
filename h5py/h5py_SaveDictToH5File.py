fa81efcea025685f1e90fb43dc75665e376804f8
eric yang 420 <eric420@gmail.com>
2021-01-02 16:38:16 +0800

    Create h5py_SaveDictToH5File.py

---
+++ h5py/h5py_SaveDictToH5File.py
@@ -0,0 +1,13 @@
+#import h5py library
+import h5py
+
+#Create Dictionary
+languages = {
+    'name': 'python',
+    'version': '3.5'
+}
+
+#for each element in dictionary, create dataset by dictionary key (name) & dictionary value (version)
+with h5py.File('test.h5','w') as hf:
+    for element in languages.keys():
+        hf.create_dataset(element, data=languages[element])
